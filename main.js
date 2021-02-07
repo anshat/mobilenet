5723d5c54aac7e0bf16cac80a43007efa61c208d
@nsh@ <46368312+anshat@users.noreply.github.com>
2021-02-07 12:07:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+Webcam.set({
+    width:310,
+    height:300,
+    image_format:'png',
+    png_quality:90,
+    constraints:{
+        facingMode:"enviorment"
+    }
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_img" src="'+data_uri+'">'
+    })
+}
+console.log('ml5 version',ml5.version);
+classifier=ml5.imageClassifier('mobileNet',modelLoaded);
+function modelLoaded(){
+    console.log("model loaded");
+}
+function check(){
+    img=document.getElementById('captured_img');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,result){
+    if(error){
+    console.error(error)
+    }
+    else{
+        console.log(result)
+        document.getElementById("object_name").innerHTML=results[0].label;
+    }
+}
